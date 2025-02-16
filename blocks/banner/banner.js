4bb633d929adaf6b054951371b4c7911b216b85c
bhargavi-chitti <163282496+bhargavi-chitti@users.noreply.github.com>
2025-02-16 18:03:55 +0530

    added hover and colours

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,23 +1,33 @@
 import { decorateButtons } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const linkElement = block.querySelector('a');
-  const textElement = block.querySelector('div > div:nth-child(2) > div');
-
   const bannerContent = document.createElement('div');
   bannerContent.className = 'banner-content';
 
-  const bannerLink = document.createElement('div');
-  bannerLink.className = 'banner-link';
-  bannerLink.appendChild(linkElement);
+  const textContainer = document.createElement('div');
+  textContainer.className = 'text-container';
 
-  const bannerText = document.createElement('div');
-  bannerText.className = 'banner-text';
-  bannerText.textContent = textElement.textContent;
+  const header = document.createElement('h1');
+  header.innerHTML = 'Banner <span class="white-text">Section</span> <span class="multi-color-text">Try it now</span>';
+  textContainer.appendChild(header);
 
-  bannerContent.appendChild(bannerLink);
-  bannerContent.appendChild(bannerText);
+  const description = document.createElement('p');
+  description.textContent = "Use Project Helix's serverless architecture to meet any traffic need. Use Project Helix's PageSpeed Insights Github action to evaluate every Pull-Request for Lighthouse Score.";
+  textContainer.appendChild(description);
 
+  const linkContainer = document.createElement('p');
+  linkContainer.className = 'banner-link';
+  const linkWrapper = document.createElement('div');
+  linkWrapper.className = 'link-wrapper';
+  const link = document.createElement('a');
+  link.href = 'https://www.amazon.in/';
+  link.innerHTML = '<span class="cta-icon">✨</span> Try now with Installing';
+  linkWrapper.appendChild(link);
+  linkContainer.appendChild(linkWrapper);
+
+  textContainer.appendChild(linkContainer);
+
+  bannerContent.appendChild(textContainer);
   block.innerHTML = '';
   block.appendChild(bannerContent);
 
