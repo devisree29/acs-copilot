4bc15f962270abc42024eea0fee46e0908e36e6b
schagalli <schagalli@adobe.com>
2025-02-21 15:58:30 +0530

    Initial commit for the footer changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,19 +2,33 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
- * loads and decorates the footer
+ * Loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  // Load footer fragment
+  const footerPath = getMetadata('footer') 
+    ? new URL(getMetadata('footer'), window.location).pathname 
+    : '/footer';
   const fragment = await loadFragment(footerPath);
 
-  // decorate footer DOM
+  if (!fragment) return;
+
+  // Replace block content with footer fragment
   block.textContent = '';
   const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
+  footer.append(...fragment.children); // Appends all child elements efficiently
   block.append(footer);
+
+  // Add "Back to Top" button
+  const lastPara = footer.querySelector('p');
+  if (lastPara) {
+    lastPara.insertAdjacentHTML('beforeend', `
+      <span class="icon icon-up-arrow-dark-theme">
+        <a href="#" class="back-to-top-link">
+          <img src="../../icons/up-arrow-dark-theme.png" class="back-to-top-icon" alt="Back to Top">
+        </a>
+      </span>
+    `);
+  }
 }
