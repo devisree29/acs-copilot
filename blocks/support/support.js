07696981a7e73bcf5ea55996731f0569923ab15b
SRINIVAS B N . <srin@adobe.com>
2025-02-24 14:07:33 +0530

    added comments1

--- blocks/support/support.js
+++ blocks/support/support.js
@@ -8,11 +8,8 @@ import createField from './form-fields.js';
 async function createForm(formHref) {
   const response = await fetch(new URL(formHref).pathname);
   const { data } = await response.json();
-  
   const form = document.createElement('form');
-  
   (await Promise.all(data.map((fd) => createField(fd, form)))).forEach((field) => field && form.append(field));
-  
   return form;
 }
 
