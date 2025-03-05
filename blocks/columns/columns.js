d91247f9464523795eaae4870a174de872385073
Apoorva Hegde <apoorva1@adobe.com>
2025-03-05 10:25:14 +0530

    add metric block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -13,6 +13,25 @@ export default function decorate(block) {
           picWrapper.classList.add('columns-img-col');
         }
       }
+
+      if (block.classList.contains('metric')) {
+        const counter = col.querySelector('h4');
+        if (counter && !isNaN(parseInt(counter.textContent, 10))) {
+          window.addEventListener("load", () => makeCounter(counter, 3000));
+        }
+      }
     });
   });
+}
+
+export function makeCounter(el, duration) {
+  const target = parseInt(el.textContent, 10) || 0;
+  if (!target) return;
+  const start = performance.now();
+
+  (function updateCounter(time) {
+    const progress = Math.min((time - start) / duration, 1);
+    el.textContent = Math.floor(progress * target);
+    if (progress < 1) requestAnimationFrame(updateCounter);
+  })(start);
 }
