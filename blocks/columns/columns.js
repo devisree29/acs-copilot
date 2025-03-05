53072942956a2d5a7d505ae3eca4bef9d92bdd03
Apoorva Hegde <apoorva1@adobe.com>
2025-03-05 12:20:24 +0530

    add metric block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,15 +1,3 @@
-export function makeCounter(el, duration) {
-  const target = parseInt(el.textContent, 10) || 0;
-  if (!target) return;
-  const start = performance.now();
-
-  (function updateCounter(time) {
-    const progress = Math.min((time - start) / duration, 1);
-    el.textContent = Math.floor(progress * target);
-    if (progress < 1) requestAnimationFrame(updateCounter);
-  }(start));
-}
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -23,13 +11,6 @@ export default function decorate(block) {
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
-        }
-      }
-
-      if (block.classList.contains('metric')) {
-        const counter = col.querySelector('h4');
-        if (counter && !Number.isNaN(parseInt(counter.textContent, 10))) {
-          window.addEventListener('load', () => makeCounter(counter, 3000));
         }
       }
     });
---
+++ blocks/metric/metric.js
@@ -0,0 +1,21 @@
+export function makeCounter(el, target, textBefore = '', textAfter = '', duration = 2000) {
+  const start = performance.now();
+  (function updateCounter(time) {
+    const progress = Math.min((time - start) / duration, 1);
+    el.textContent = `${textBefore}${Math.floor(progress * target)}${textAfter}`;
+    if (progress < 1) requestAnimationFrame(updateCounter);
+  }(start));
+}
+
+export default function decorate(block) {
+  [...block.children].forEach((row) => [...row.children].forEach((col) => {
+    const counter = col.firstElementChild;
+    const number = counter?.textContent.match(/\d+/);
+    if (number) {
+      const target = parseInt(number[0], 10);
+      const textBefore = counter.textContent.split(number[0])[0];
+      const textAfter = counter.textContent.split(number[0])[1];
+      makeCounter(counter, target, textBefore, textAfter);
+    }
+  }));
+}
