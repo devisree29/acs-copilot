1c9cdb62bf91de5cfa84d0dea040a0e962e99ada
Manish Jha <manishjha@adobe.com>
2025-03-06 12:40:49 +0530

    accordion 156 7

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -40,9 +40,7 @@ export default function decorate(block) {
               allQuestions.push({ question: ans.innerHTML.trim(), answers: [] });
             } else if (ans.tagName === 'P') {
               if (allQuestions.length > 0) {
-                allQuestions[allQuestions.length - 1].answers.push(
-                  ans.innerHTML.trim()
-                );
+                allQuestions[allQuestions.length - 1].answers.push(ans.innerHTML.trim());
               }
             }
           }
@@ -53,11 +51,8 @@ export default function decorate(block) {
     // Split questions into three separate containers
     const splitQuestions = [
       allQuestions.slice(0, Math.ceil(allQuestions.length / 3)),
-      allQuestions.slice(
-        Math.ceil(allQuestions.length / 3),
-        2 * Math.ceil(allQuestions.length / 3)
-      ),
-      allQuestions.slice(2 * Math.ceil(allQuestions.length / 3))
+      allQuestions.slice(Math.ceil(allQuestions.length / 3), 2 * Math.ceil(allQuestions.length / 3)),
+      allQuestions.slice(2 * Math.ceil(allQuestions.length / 3)),
     ];
 
     categories.push({ title: categoryTitleText, questionsGroup: splitQuestions });
@@ -77,9 +72,7 @@ export default function decorate(block) {
     categoryTitle.appendChild(categoryIcon);
 
     categoryTitle.addEventListener('click', () => {
-      const expanded =
-        questionList.style.display === 'block' &&
-        categoryTitle.classList.contains('active');
+      const expanded = questionList.style.display === 'block' && categoryTitle.classList.contains('active');
 
       document.querySelectorAll('.faq-category-title').forEach((el) => {
         el.classList.remove('active');
@@ -102,8 +95,7 @@ export default function decorate(block) {
             questionItem.classList.add('faq-header');
             questionItem.innerHTML = `
               <span class='faq-question'>${qa.question}</span>
-              <span class='faq-icon'>+</span>
-            `;
+              <span class='faq-icon'>+</span>`;
 
             const answerContainer = document.createElement('div');
             answerContainer.classList.add('faq-answer-container');
